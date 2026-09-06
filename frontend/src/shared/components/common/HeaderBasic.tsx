baea30cfaf8153bbb5503a76ef987ca7c88bb810
CarlosGunter <carlosgt1702@gmail.com>
2026-09-06 01:31:33 -0600

    feat: se agregaron nuevos colores de acento y se actualizaron los enlaces en el Header Sencillo

--- frontend/src/shared/components/common/HeaderBasic.tsx
+++ frontend/src/shared/components/common/HeaderBasic.tsx
@@ -13,12 +13,12 @@ export function HeaderBasic() {
           <MACTILogo className="w-full h-auto" />
         </Link>
         <nav className="text-lg font-medium flex items-center gap-4 md:gap-6">
-          <Link href="/faq" className="hover:text-accent-foreground transition">
+          <Link href="/faq" className="hover:text-accent-invert-foreground transition">
             FAQ
           </Link>
           <Link
             href="https://www.geofisica.unam.mx/recursos/docs/IGEF_aviso_privacidad_20190802.pdf"
-            className="hover:text-accent-foreground transition leading-6 w-min md:w-auto"
+            className="hover:text-accent-invert-foreground transition leading-6 w-min md:w-auto"
           >
             Aviso de Privacidad
           </Link>
