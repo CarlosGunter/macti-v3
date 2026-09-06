64c017a2ba08d7419bbfe25478d0c3d2bf5831ad
CarlosGunter <carlosgt1702@gmail.com>
2026-09-05 23:43:47 -0600

    feat: se actualizaron estilos y contenido de los componentes para el nuevo diseño

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,3 @@
-import Hero from "@/domains/home/components/Hero";
 import InstituteCard from "@/domains/home/components/InstituteCard";
 import { HeaderBasic } from "@/shared/components/common/HeaderBasic";
 import { institutes } from "@/shared/config/institutes";
@@ -9,15 +8,14 @@ export default function Home() {
       <HeaderBasic />
       <div className="max-w-7xl mx-auto p-2 my-6 w-full">
         <main className="grid gap-4 w-full">
-          <h2 className="text-4xl font-bold">Institutos</h2>
-          <section className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:grid-rows-3 lg:[&>*:nth-child(4)]:col-span-2 lg:[&>*:last-child]:col-span-2">
+          <h2 className="text-4xl font-bold">Explorar Dependencias</h2>
+          <section className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
             {Object.entries(institutes).map(([key, institute]) => (
               <InstituteCard key={key} id={key} {...institute} />
             ))}
           </section>
         </main>
       </div>
-      <Hero />
     </>
   );
 }
