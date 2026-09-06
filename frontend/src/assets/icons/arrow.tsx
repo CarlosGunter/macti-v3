294861b7e82c57acadf63f40b0a2ba4b66fea3be
CarlosGunter <carlosgt1702@gmail.com>
2026-09-06 01:44:52 -0600

    fix: se corrigieron las props de la flecha svg

--- frontend/src/assets/icons/arrow.tsx
+++ frontend/src/assets/icons/arrow.tsx
@@ -1,16 +1,17 @@
 export function Arrow({ className = "" }: { className?: string }) {
   return (
     <svg
-      className={`w-5 h-5 transform group-hover:translate-x-1 transition-transform ${className}`}
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
+      strokeWidth="2"
       viewBox="0 0 24 24"
+      className={className}
+      xmlns="http://www.w3.org/2000/svg"
     >
       <title>Arrow</title>
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         d="M14 5l7 7m0 0l-7 7m7-7H3"
       ></path>
     </svg>
